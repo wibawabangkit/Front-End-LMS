c56d6d0c7fa67b53319a8f2f3ddc9e7821265c39
Wibawa Bangkit <wibawabangkit31@gmail.com>
2021-04-12 16:19:56 +0700

    revisi

--- routes/web.php
+++ routes/web.php
@@ -10,23 +10,25 @@ Route::get('/boost', function () {
     dd('OK');
 });
 
-Route::redirect('/', '/dashboard', 301);
+route::redirect('/', '/dashboard', 301);
 
-Route::get('/dashboard', [\App\Http\Controllers\Auth\LoginController::class, 'index'])->name('auth.index');
+route::prefix('dasboard')->group(function () {
+    route::get('/', [\App\Http\Controllers\Auth\LoginController::class, 'index'])->name('auth.index');
 
-route::prefix('headmaster')->group(function () {
-    Route::get('/calender', [\App\Http\Controllers\Headmaster\DashboardController::class, 'calender'])->name('calender.index');
-    Route::get('/statistik', [\App\Http\Controllers\Headmaster\DashboardController::class, 'statistik'])->name('statistik.index');
-    Route::get('/class', [\App\Http\Controllers\Headmaster\DashboardController::class, 'class'])->name('class.index');
-});
+    route::prefix('headmaster')->group(function () {
+        Route::get('/calender', [\App\Http\Controllers\Headmaster\DashboardController::class, 'calender'])->name('calender.index');
+        Route::get('/statistik', [\App\Http\Controllers\Headmaster\DashboardController::class, 'statistik'])->name('statistik.index');
+        Route::get('/class', [\App\Http\Controllers\Headmaster\DashboardController::class, 'class'])->name('class.index');
+    });
 
-route::prefix('student')->group(function () {
-    Route::get('/dashboard', [\App\Http\Controllers\Student\DashboardController::class, 'dashbord'])->name('dashboard.index');
-    Route::get('/transkip-nilai', [\App\Http\Controllers\Student\DashboardController::class, 'transkipNilai'])->name('transkip-nilai.index');
-});
+    route::prefix('student')->group(function () {
+        Route::get('/dashboard', [\App\Http\Controllers\Student\DashboardController::class, 'dashbord'])->name('dashboard.index');
+        Route::get('/transkip-nilai', [\App\Http\Controllers\Student\DashboardController::class, 'transkipNilai'])->name('transkip-nilai.index');
+    });
 
-route::prefix('teacher')->group(function () {
-    Route::get('/calendar-akademic', [\App\Http\Controllers\Teacher\DashboardController::class, 'calendar'])->name('teacher.calendar-akademic.index');
-    Route::get('/class', [\App\Http\Controllers\Teacher\DashboardController::class, 'overviewClass'])->name('teacher.class.index');
-    Route::get('/teacher', [\App\Http\Controllers\Teacher\DashboardController::class, 'overviewTeacher'])->name('teacher.index');
+    route::prefix('teacher')->group(function () {
+        Route::get('/calendar-akademic', [\App\Http\Controllers\Teacher\DashboardController::class, 'calendar'])->name('teacher.calendar-akademic.index');
+        Route::get('/class', [\App\Http\Controllers\Teacher\DashboardController::class, 'overviewClass'])->name('teacher.class.index');
+        Route::get('/teacher', [\App\Http\Controllers\Teacher\DashboardController::class, 'overviewTeacher'])->name('teacher.index');
+    });
 });
